1955d43295fab9f97c172a4b9ad5c6dc6cf963e0
AbigailAda14 <abigailezeugwu@gmail.com>
2023-04-28 16:21:29 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,15 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
